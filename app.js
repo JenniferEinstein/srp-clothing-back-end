c227aa63f6758cc4ae389b0e7a90f9aca7e2ecd6
JenniferEinstein <jennifereinstein@gmail.com>
2023-08-08 20:50:47 -0400

    Added psuedo homepage to back end as requested. Put in buttons but no functionality. Installed bootstrap.

--- app.js
+++ app.js
@@ -13,9 +13,15 @@ app.use(express.json());
 
 // === ROUTES ===
 
-app.get("/", (req, res) => {
-    res.send("Welcome to the Clothing Emporium Extravaganza [Back-End]");
+app.get("/", (req, res) => {  res.send(
+    "<html><head>This is the head component</head><body><h1>Welcome to the Clothing Emporium Extravaganza [Back-End]</h1></br><h3><button type='button' className='btn btn-primary'>See all clothing</button></br></br><button type='button' className='btn btn-primary' >Add an item of clothing to the database</button></br></br><button type='button' className='btn btn-primary'>Delete an item of clothing from the database</button></br></br><button type='button' className='btn btn-primary'>Update an item of clothing</button></br></br><button type='button' className='btn btn-primary'>See one piece of the clothing</button></h3></body></html>"
+    );
 });
+
+
+
+
+
 
 
 // clothing ROUTES (implement the index route that uses pg-promise to query your db)
