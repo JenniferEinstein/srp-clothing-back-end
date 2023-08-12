2ce2e6cb5c2dca5fdbfd221e37d00abf8d00253f
Starr <Starr@M-C02F21YMMD6R.nordstrom.net>
2023-08-12 13:05:47 -0400

    updated the product types/categories

--- db/seed.sql
+++ db/seed.sql
@@ -3,8 +3,8 @@
 INSERT INTO clothing (name, category, cost, in_stock, country, handwash_only, material, created_on, username)
 VALUES
 -- Women's Tops
-('Butterfly top', 'womens-top', 34.95, true, 'Spain', false, 'cotton, rayon', '8/05/2023', 'Jennifer'),
-('For Every Season top', 'womens-top', 25.00, true, 'Viet nam', false, 'cotton, polyester', '8/05/2023', 'Jennifer'),
+('Butterfly top', 'womens-tops', 34.95, true, 'Spain', false, 'cotton, rayon', '8/05/2023', 'Jennifer'),
+('For Every Season top', 'womens-tops', 25.00, true, 'Viet nam', false, 'cotton, polyester', '8/05/2023', 'Jennifer'),
 -- Women's Shoes
 ('Red Boots', 'womens-shoes', 104.9, true, 'Turkey', true, 'leather, rubber', '8/05/2023', 'Jennifer'),
 -- Women's Accessories
@@ -15,22 +15,22 @@ VALUES
 -- Women's Bottoms
 ('Reticulated Jeans', 'womens-bottoms', 45.00, true, 'China', false, 'denim', '8/05/2023', 'Jennifer'),
 -- Women's Dresses
-('Sunshine Sundress', 'womens-dress', 145.25, true, 'Mexico', false, 'cotton', '8/05/2023', 'Jennifer'),
-('Crinkled midiskirt', 'womens-dress', 45.00, false, 'USA', false, 'cotton', '8/05/2023', 'Jennifer'),
+('Sunshine Sundress', 'womens-dresses', 145.25, true, 'Mexico', false, 'cotton', '8/05/2023', 'Jennifer'),
+('Crinkled midiskirt', 'womens-dresses', 45.00, false, 'USA', false, 'cotton', '8/05/2023', 'Jennifer'),
 -- Men's Tops
-('Box Cut Tee', 'mens-top', 30.00, true, 'Turkey', true, 'cotton, polyester', '07/04/2023', 'Sarai'),
-('Soft Wash Polo', 'mens-top', 35.99, true, 'China', true, 'cotton, polyester', '07/04/2023', 'Sarai'),
-('Men/s Linen Shirt Button Up', 'mens-top', 60.00, true, 'China', true, 'linen', '07/04/2023', 'Sarai'),
-('Scrunch Sleeve Sweatshirt', 'mens-top', 70.00, true, 'China', true, 'cotton, polyester', '07/04/2023', 'Sarai'),
+('Box Cut Tee', 'mens-tops', 30.00, true, 'Turkey', true, 'cotton, polyester', '07/04/2023', 'Sarai'),
+('Soft Wash Polo', 'mens-tops', 35.99, true, 'China', true, 'cotton, polyester', '07/04/2023', 'Sarai'),
+('Men/s Linen Shirt Button Up', 'mens-tops', 60.00, true, 'China', true, 'linen', '07/04/2023', 'Sarai'),
+('Scrunch Sleeve Sweatshirt', 'mens-tops', 70.00, true, 'China', true, 'cotton, polyester', '07/04/2023', 'Sarai'),
 -- Men's Bottoms
-('Men/s Ripped Jeans', 'mens-bottom', 300.00, false, 'Bangladesh', true, 'demin, polyester', '07/05/2023', 'Sarai'),
-('Tailored Cut Chinos', 'mens-bottom', 300.00, false, 'Bangladesh', true, 'cotton, polyester', '07/04/2023', 'Sarai'),
-('Slim Fit Jean Pant', 'mens-bottom', 300.00, false, 'Bangladesh', true, 'demin, polyester', '07/06/2023', 'Sarai'),
+('Men/s Ripped Jeans', 'mens-bottoms', 300.00, false, 'Bangladesh', true, 'demin, polyester', '07/05/2023', 'Sarai'),
+('Tailored Cut Chinos', 'mens-bottoms', 300.00, false, 'Bangladesh', true, 'cotton, polyester', '07/04/2023', 'Sarai'),
+('Slim Fit Jean Pant', 'mens-bottoms', 300.00, false, 'Bangladesh', true, 'demin, polyester', '07/06/2023', 'Sarai'),
 -- Men's Accessories 
-('Designer Leather Belt Medium Decal', 'mens-belt', 500.00, true, 'Bangladesh', true, 'leather, metal, gold plated', '03/24/2023', 'Sarai'),
-('Designer Leather Belt Small Decal', 'mens-belt', 435.00, true, 'Bangladesh', true, 'leather, metal, gold plated', '03/24/2023', 'Sarai'),
-('Knee-Hight Crew Socks ', 'mens-sock', 10.00, true, 'China', false, 'cotton, polyester', '07/09/2022', 'Sarai'),
-('Structured Fidora', 'mens-hat', 25.00, true, 'China', false, 'cotton, polyester', '04/01/2022', 'Sarai'),
+('Designer Leather Belt Medium Decal', 'mens-belts', 500.00, true, 'Bangladesh', true, 'leather, metal, gold plated', '03/24/2023', 'Sarai'),
+('Designer Leather Belt Small Decal', 'mens-belts', 435.00, true, 'Bangladesh', true, 'leather, metal, gold plated', '03/24/2023', 'Sarai'),
+('Knee-Hight Crew Socks ', 'mens-socks', 10.00, true, 'China', false, 'cotton, polyester', '07/09/2022', 'Sarai'),
+('Structured Fidora', 'mens-hats', 25.00, true, 'China', false, 'cotton, polyester', '04/01/2022', 'Sarai'),
 -- Men's Shoes 
 ('Cool Loafer', 'mens-shoes', 250.00, true, 'India', true, 'Suede, Leather', '04/09/2022', 'Sarai'),
 ('Oxford Dress Shoes', 'mens-shoes', 315.00, false, 'Turkey', false, 'Suede, Nubuck',' 06/15/2021', 'Sarai'),
